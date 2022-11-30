833c36b102999d4ee66b83fe82a0424ee7e784d9
muhammadhanif71 <118676379+muhammadhanif71@users.noreply.github.com>
2022-12-01 00:31:42 +0700

    xman

---
+++ userbot/modules/helper.py
@@ -0,0 +1,41 @@
+""" Userbot module for other small commands. """
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP
+from userbot.utils import edit_or_reply, man_cmd
+
+
+@man_cmd(pattern="ihelp$")
+async def usit(event):
+    me = await event.client.get_me()
+    await edit_or_reply(
+        event,
+        f"**Hai {me.first_name} Kalo Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
+        f"✣ **Group Support :** [Stuf Support](t.me/stufsupport)\n"
+        f"✣ **Channel Man :** [Stuf Channel](t.me/stufchannel)\n"
+        f"✣ **Owner Repo :** [Itachi](t.me/kalijogo)\n"
+        f"✣ **Repo :** [xMan-Userbot](https://github.com/muhammadhanif71/xMan-Userbot)\n",
+    )
+
+
+@man_cmd(pattern="listvar$")
+async def var(event):
+    await edit_or_reply(
+        event,
+        "**Daftar Lengkap Vars Dari xMan-Userbot:** [KLIK DISINI](https://telegra.ph/List-Variabel-Heroku-untuk-Man-Userbot-09-22)",
+    )
+
+
+CMD_HELP.update(
+    {
+        "helper": f"**Plugin : **`helper`\
+        \n\n  •  **Syntax :** `{cmd}ihelp`\
+        \n  •  **Function : **Bantuan Untuk Man-Userbot.\
+        \n\n  •  **Syntax :** `{cmd}listvar`\
+        \n  •  **Function : **Melihat Daftar Vars.\
+        \n\n  •  **Syntax :** `{cmd}repo`\
+        \n  •  **Function : **Melihat Repository Man-Userbot.\
+        \n\n  •  **Syntax :** `{cmd}string`\
+        \n  •  **Function : **Link untuk mengambil String Man-Userbot.\
+    "
+    }
+)
