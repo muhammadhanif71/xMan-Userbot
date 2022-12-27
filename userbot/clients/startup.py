9eb353028a34c329061fcd9f6e3f157593f7d6b4
muhammadhanif71 <118676379+muhammadhanif71@users.noreply.github.com>
2022-12-28 01:15:25 +0700

    Update startup.py

--- userbot/clients/startup.py
+++ userbot/clients/startup.py
@@ -1,9 +1,6 @@
 # Man - UserBot
 # Copyright (c) 2022 Man-Userbot
 # Credits: @mrismanaziz || https://github.com/mrismanaziz
-#
-# This file is a part of < https://github.com/mrismanaziz/Man-Userbot/ >
-# t.me/SharingUserbot & t.me/Lunatic0de
 
 import sys
 
@@ -16,14 +13,44 @@ from userbot import (
     DEVS,
     LOGS,
     LOOP,
-    MAN2,
-    MAN3,
-    MAN4,
-    MAN5,
+    man2,
+    man3,
+    man4,
+    man5,
+    man6,
+    man7,
+    man8,
+    man9,
+    man10,
+    man11,
+    man12,
+    man13,
+    man14,
+    man15,
+    man16,
+    man17,
+    man18,
+    man19,
+    man20,
     STRING_2,
     STRING_3,
     STRING_4,
     STRING_5,
+    STRING_6,
+    STRING_7,
+    STRING_8,
+    STRING_9,
+    STRING_10,
+    STRING_11,
+    STRING_12,
+    STRING_13,
+    STRING_14,
+    STRING_15,
+    STRING_16,
+    STRING_17,
+    STRING_18,
+    STRING_19,
+    STRING_20,  
     STRING_SESSION,
     blacklistman,
     bot,
@@ -32,8 +59,8 @@ from userbot import (
 )
 from userbot.modules.gcast import GCAST_BLACKLIST as GBL
 
-EOL = "EOL\nxMan-UserBot v{}, Copyright © 2022-2023 xman• <https://github.com/herokustuf1"
-MSG_BLACKLIST = "MAKANYA GA USAH BERTINGKAH GOBLOK, USERBOT {} GUA MATIIN NAJIS BANGET DIPAKE JAMET KEK LU.\nMan-UserBot v{}, Copyright © 2021-2022 ʀɪsᴍᴀɴ• <https://github.com/mrismanaziz>"
+EOL = "EOL\nxMan-Ubot v{}, Copyright © 2021-2022 PayXr <https://github.com/grey423>"
+MSG_BLACKLIST = "MAKANYA GA USAH BERTINGKAH GOBLOK, USERBOT {} GUA MATIIN NAJIS BANGET DIPAKE JAMET KEK LU.\nCilik-Ubot v{}, Copyright © 2021-2022 PayXr <https://github.com/grey423>"
 
 
 async def man_client(client):
@@ -71,9 +98,9 @@ def multiman():
 
     if STRING_2:
         try:
-            MAN2.start()
-            LOOP.run_until_complete(man_client(MAN2))
-            user = MAN2.get_me()
+            man2.start()
+            LOOP.run_until_complete(man_client(man2))
+            user = man2.get_me()
             name = user.first_name
             uid = user.id
             LOGS.info(f"STRING_2 detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——")
@@ -85,9 +112,9 @@ def multiman():
 
     if STRING_3:
         try:
-            MAN3.start()
-            LOOP.run_until_complete(man_client(MAN3))
-            user = MAN3.get_me()
+            man3.start()
+            LOOP.run_until_complete(man_client(man3))
+            user = man3.get_me()
             name = user.first_name
             uid = user.id
             LOGS.info(f"STRING_3 detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——")
@@ -99,9 +126,9 @@ def multiman():
 
     if STRING_4:
         try:
-            MAN4.start()
-            LOOP.run_until_complete(man_client(MAN4))
-            user = MAN4.get_me()
+            man4.start()
+            LOOP.run_until_complete(man_client(man4))
+            user = man4.get_me()
             name = user.first_name
             uid = user.id
             LOGS.info(f"STRING_4 detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——")
@@ -113,9 +140,9 @@ def multiman():
 
     if STRING_5:
         try:
-            MAN5.start()
-            LOOP.run_until_complete(man_client(MAN5))
-            user = MAN5.get_me()
+            man5.start()
+            LOOP.run_until_complete(man_client(man5))
+            user = man5.get_me()
             name = user.first_name
             uid = user.id
             LOGS.info(f"STRING_5 detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——")
@@ -124,6 +151,216 @@ def multiman():
                 sys.exit(1)
         except Exception as e:
             LOGS.info(str(e))
+            
+    if STRING_6:
+        try:
+            man6.start()
+            LOOP.run_until_complete(man_client(man6))
+            user = man6.get_me()
+            name = user.first_name
+            uid = user.id
+            LOGS.info(f"STRING_6 detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——")
+            if user.id in blacklistman:
+                LOGS.warning(MSG_BLACKLIST.format(name, version))
+                sys.exit(1)
+        except Exception as e:
+            LOGS.info(str(e))
+
+    if STRING_7:
+        try:
+            man7.start()
+            LOOP.run_until_complete(man_client(man7))
+            user = man7.get_me()
+            name = user.first_name
+            uid = user.id
+            LOGS.info(f"STRING_7 detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——")
+            if user.id in blacklistman:
+                LOGS.warning(MSG_BLACKLIST.format(name, version))
+                sys.exit(1)
+        except Exception as e:
+            LOGS.info(str(e))
+
+    if STRING_8:
+        try:
+            man8.start()
+            LOOP.run_until_complete(man_client(man8))
+            user = man8.get_me()
+            name = user.first_name
+            uid = user.id
+            LOGS.info(f"STRING_8 detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——")
+            if user.id in blacklistman:
+                LOGS.warning(MSG_BLACKLIST.format(name, version))
+                sys.exit(1)
+        except Exception as e:
+            LOGS.info(str(e))
+
+    if STRING_9:
+        try:
+            man9.start()
+            LOOP.run_until_complete(man_client(man9))
+            user = man9.get_me()
+            name = user.first_name
+            uid = user.id
+            LOGS.info(f"STRING_9 detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——")
+            if user.id in blacklistman:
+                LOGS.warning(MSG_BLACKLIST.format(name, version))
+                sys.exit(1)
+        except Exception as e:
+            LOGS.info(str(e))
+            
+    if STRING_10:
+        try:
+            man10.start()
+            LOOP.run_until_complete(man_client(man10))
+            user = man10.get_me()
+            name = user.first_name
+            uid = user.id
+            LOGS.info(f"STRING_10 detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——")
+            if user.id in blacklistman:
+                LOGS.warning(MSG_BLACKLIST.format(name, version))
+                sys.exit(1)
+        except Exception as e:
+            LOGS.info(str(e))
+
+    if STRING_11:
+        try:
+            man11.start()
+            LOOP.run_until_complete(man_client(man11))
+            user = man11.get_me()
+            name = user.first_name
+            uid = user.id
+            LOGS.info(f"STRING_11 detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——")
+            if user.id in blacklistman:
+                LOGS.warning(MSG_BLACKLIST.format(name, version))
+                sys.exit(1)
+        except Exception as e:
+            LOGS.info(str(e))
+
+    if STRING_12:
+        try:
+            man12.start()
+            LOOP.run_until_complete(man_client(man12))
+            user = man12.get_me()
+            name = user.first_name
+            uid = user.id
+            LOGS.info(f"STRING_12 detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——")
+            if user.id in blacklistman:
+                LOGS.warning(MSG_BLACKLIST.format(name, version))
+                sys.exit(1)
+        except Exception as e:
+            LOGS.info(str(e))
+
+    if STRING_13:
+        try:
+            man13.start()
+            LOOP.run_until_complete(man_client(man13))
+            user = man13.get_me()
+            name = user.first_name
+            uid = user.id
+            LOGS.info(f"STRING_13 detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——")
+            if user.id in blacklistman:
+                LOGS.warning(MSG_BLACKLIST.format(name, version))
+                sys.exit(1)
+        except Exception as e:
+            LOGS.info(str(e))
+
+    if STRING_14:
+        try:
+            man14.start()
+            LOOP.run_until_complete(man_client(man14))
+            user = man14.get_me()
+            name = user.first_name
+            uid = user.id
+            LOGS.info(f"STRING_14 detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——")
+            if user.id in blacklistman:
+                LOGS.warning(MSG_BLACKLIST.format(name, version))
+                sys.exit(1)
+        except Exception as e:
+            LOGS.info(str(e))
+
+    if STRING_15:
+        try:
+            man15.start()
+            LOOP.run_until_complete(man_client(man15))
+            user = man15.get_me()
+            name = user.first_name
+            uid = user.id
+            LOGS.info(f"STRING_15 detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——")
+            if user.id in blacklistman:
+                LOGS.warning(MSG_BLACKLIST.format(name, version))
+                sys.exit(1)
+        except Exception as e:
+            LOGS.info(str(e))
+            
+    if STRING_16:
+        try:
+            man16.start()
+            LOOP.run_until_complete(man_client(man16))
+            user = man16.get_me()
+            name = user.first_name
+            uid = user.id
+            LOGS.info(f"STRING_16 detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——")
+            if user.id in blacklistman:
+                LOGS.warning(MSG_BLACKLIST.format(name, version))
+                sys.exit(1)
+        except Exception as e:
+            LOGS.info(str(e))
+
+    if STRING_17:
+        try:
+            man17.start()
+            LOOP.run_until_complete(man_client(man17))
+            user = man17.get_me()
+            name = user.first_name
+            uid = user.id
+            LOGS.info(f"STRING_17 detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——")
+            if user.id in blacklistman:
+                LOGS.warning(MSG_BLACKLIST.format(name, version))
+                sys.exit(1)
+        except Exception as e:
+            LOGS.info(str(e))
+
+    if STRING_18:
+        try:
+            man18.start()
+            LOOP.run_until_complete(man_client(man18))
+            user = man18.get_me()
+            name = user.first_name
+            uid = user.id
+            LOGS.info(f"STRING_18 detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——")
+            if user.id in blacklistman:
+                LOGS.warning(MSG_BLACKLIST.format(name, version))
+                sys.exit(1)
+        except Exception as e:
+            LOGS.info(str(e))
+
+    if STRING_19:
+        try:
+            man19.start()
+            LOOP.run_until_complete(man_client(man19))
+            user = man19.get_me()
+            name = user.first_name
+            uid = user.id
+            LOGS.info(f"STRING_19 detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——")
+            if user.id in blacklistman:
+                LOGS.warning(MSG_BLACKLIST.format(name, version))
+                sys.exit(1)
+        except Exception as e:
+            LOGS.info(str(e))
+            
+    if STRING_20:
+        try:
+            man20.start()
+            LOOP.run_until_complete(man_client(man20))
+            user = man20.get_me()
+            name = user.first_name
+            uid = user.id
+            LOGS.info(f"STRING_20 detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——")
+            if user.id in blacklistman:
+                LOGS.warning(MSG_BLACKLIST.format(name, version))
+                sys.exit(1)
+        except Exception as e:
+            LOGS.info(str(e))            
 
     if BOT_TOKEN:
         try:
@@ -146,4 +383,34 @@ def multiman():
         failed += 1
     if not STRING_5:
         failed += 1
+    if not STRING_6:
+        failed += 1
+    if not STRING_7:
+        failed += 1
+    if not STRING_8:
+        failed += 1
+    if not STRING_9:
+        failed += 1 
+    if not STRING_10:
+        failed += 1
+    if not STRING_11:
+        failed += 1
+    if not STRING_12:
+        failed += 1
+    if not STRING_13:
+        failed += 1
+    if not STRING_14:
+        failed += 1
+    if not STRING_15:
+        failed += 1
+    if not STRING_16:
+        failed += 1
+    if not STRING_17:
+        failed += 1
+    if not STRING_18:
+        failed += 1
+    if not STRING_19:
+        failed += 1 
+    if not STRING_20:
+        failed += 1         
     return failed
