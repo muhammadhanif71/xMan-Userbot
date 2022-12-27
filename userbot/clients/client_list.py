ae0f469f2f7281c2457fe4b356aecee7deaac5f7
muhammadhanif71 <118676379+muhammadhanif71@users.noreply.github.com>
2022-12-28 00:59:37 +0700

    Update client_list.py

--- userbot/clients/client_list.py
+++ userbot/clients/client_list.py
@@ -11,7 +11,7 @@ import telethon.utils
 from telethon.tl.functions.users import GetFullUserRequest
 
 
-async def clients_list(SUDO_USERS, bot, MAN2, MAN3, MAN4, MAN5):
+async def clients_list(SUDO_USERS, bot, MAN2, MAN3, MAN4, MAN5,MAN6, MAN7, MAN8, MAN9, MAN10, MAN11, MAN12, MAN13, MAN14, MAN15, MAN16, MAN17, MAN18, MAN19, MAN20):
     user_ids = list(SUDO_USERS) or []
     main_id = await bot.get_me()
     user_ids.append(main_id.id)
@@ -39,8 +39,113 @@ async def clients_list(SUDO_USERS, bot, MAN2, MAN3, MAN4, MAN5):
 
     try:
         if MAN5 is not None:
-            id5 = await MAN5.get_me()
+           id5 = await MAN5.get_me()
             user_ids.append(id5.id)
+    except BaseException:
+        pass
+
+    try:
+        if MAN6 is not None:
+            id6 = await MAN6get_me()
+            user_ids.append(id6.id)
+    except BaseException:
+        pass
+
+    try:
+        if MAN7 is not None:
+            id7 = await MAN7.get_me()
+            user_ids.append(id7.id)
+    except BaseException:
+        pass
+
+    try:
+        if MAN8 is not None:
+            id8 = await MAN8.get_me()
+            user_ids.append(id8.id)
+    except BaseException:
+        pass
+
+    try:
+        if MAN9 is not None:
+            id9 = await MAN9.get_me()
+            user_ids.append(id9.id)
+    except BaseException:
+        pass
+
+    try:
+        if MAN10 is not None:
+            id10 = await MAN10.get_me()
+            user_ids.append(id10.id)
+    except BaseException:
+        pass
+
+    try:
+        if MAN11 is not None:
+            id11 = await MAN11.get_me()
+            user_ids.append(id11.id)
+    except BaseException:
+        pass
+
+    try:
+        if MAN12 is not None:
+            id12 = await MAN12.get_me()
+            user_ids.append(id12.id)
+    except BaseException:
+        pass
+
+    try:
+        if MAN13 is not None:
+            id13 = await MAN13.get_me()
+            user_ids.append(id13.id)
+    except BaseException:
+        pass
+
+    try:
+        if MAN14 is not None:
+            id14 = await MA14.get_me()
+            user_ids.append(id14.id)
+    except BaseException:
+        pass
+
+    try:
+        if MAN15 is not None:
+            id15 = await MAN15.get_me()
+            user_ids.append(id15.id)
+    except BaseException:
+        pass
+
+    try:
+        if MAN16 is not None:
+            id16 = await MAN16.get_me()
+            user_ids.append(id16.id)
+    except BaseException:
+        pass
+
+    try:
+        if MAN17 is not None:
+            id17 = await MAN17.get_me()
+            user_ids.append(id17.id)
+    except BaseException:
+        pass
+
+    try:
+        if MAN18 is not None:
+            id18 = await MAN18.get_me()
+            user_ids.append(id18.id)
+    except BaseException:
+        pass
+
+    try:
+        if MAN19 is not None:
+            id19 = await MAN19.get_me()
+            user_ids.append(id19.id)
+    except BaseException:
+        pass
+
+    try:
+        if MAN20 is not None:
+            id20 = await MAN20.get_me()
+            user_ids.append(id20.id)
     except BaseException:
         pass
 
