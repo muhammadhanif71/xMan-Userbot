e3fc16d6ef3f89b6f9d2d6e711c711864f7b4dbf
muhammadhanif71 <118676379+muhammadhanif71@users.noreply.github.com>
2022-12-28 10:22:20 +0700

    Update logger.py

--- userbot/clients/logger.py
+++ userbot/clients/logger.py
@@ -13,7 +13,7 @@ from telethon.tl.types import ChatAdminRights
 from userbot import BOT_VER as version
 from userbot import BOTLOG_CHATID
 from userbot import CMD_HANDLER as cmd
-from userbot import MAN2, MAN3, MAN4, MAN5, bot, branch, tgbot
+from userbot import MAN2, MAN3, MAN4, MAN5, MAN6, MAN7, MAN8, MAN9, MAN10, MAN11, MAN12, MAN13, MAN14, MAN15, MAN16, MAN17, MAN18, MAN19, MAN20, bot, branch, tgbot
 from userbot.utils import checking
 
 MSG_ON = """
@@ -101,6 +101,171 @@ async def man_userbot_on():
             await asyncio.sleep(3)
             if BOTLOG_CHATID != 0:
                 await MAN5.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN6:
+            await checking(MAN6)
+            await asyncio.sleep(3)
+            if BOTLOG_CHATID != 0:
+                await MAN6.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN7:
+            await checking(MAN7)
+            await asyncio.sleep(3)
+            if BOTLOG_CHATID != 0:
+                await MAN7.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN8:
+            await checking(MAN8)
+            await asyncio.sleep(3)
+            if BOTLOG_CHATID != 0:
+                await MAN8.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN9:
+            await checking(MAN9)
+            await asyncio.sleep(3)
+            if BOTLOG_CHATID != 0:
+                await MAN9.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN10:
+            await checking(MAN10)
+            await asyncio.sleep(3)
+            if BOTLOG_CHATID != 0:
+                await MAN10.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN11:
+            await checking(MAN11)
+            await asyncio.sleep(3)
+            if BOTLOG_CHATID != 0:
+                await MAN11.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN12:
+            await checking(MAN12)
+            await asyncio.sleep(3)
+            if BOTLOG_CHATID != 0:
+                await MAN12.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN13:
+            await checking(MAN13)
+            await asyncio.sleep(3)
+            if BOTLOG_CHATID != 0:
+                await MAN13.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN14:
+            await checking(MAN14)
+            await asyncio.sleep(3)
+            if BOTLOG_CHATID != 0:
+                await MAN14.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN15:
+            await checking(MAN15)
+            await asyncio.sleep(3)
+            if BOTLOG_CHATID != 0:
+                await MAN15.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN16:
+            await checking(MAN16)
+            await asyncio.sleep(3)
+            if BOTLOG_CHATID != 0:
+                await MAN16.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN17:
+            await checking(MAN17)
+            await asyncio.sleep(3)
+            if BOTLOG_CHATID != 0:
+                await MAN17.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN18:
+            await checking(MAN18)
+            await asyncio.sleep(3)
+            if BOTLOG_CHATID != 0:
+                await MAN18.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN19:
+            await checking(MAN19)
+            await asyncio.sleep(3)
+            if BOTLOG_CHATID != 0:
+                await MAN19.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN20:
+            await checking(MAN20)
+            await asyncio.sleep(3)
+            if BOTLOG_CHATID != 0:
+                await MAN20.send_message(
                     BOTLOG_CHATID,
                     MSG_ON.format(version, branch, cmd),
                 )
